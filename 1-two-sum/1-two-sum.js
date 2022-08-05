527a911b7406dca52ada03ba0d6e0c06c7f8ef35
mickey <damapongm@gmail.com>
2022-08-05 09:07:27 -0700

    Time: 188 ms (25.61%), Space: 42.5 MB (67.90%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+};
+
+
+// we want to add two number in the string to get the same number as target num
+//first I will iterate through array
+//if num
